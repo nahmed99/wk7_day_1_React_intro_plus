5a68b0f1a9c2d1d290b756771a82ef40db6477e3
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-14 12:30:02 +0000

    use state added comments added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ function App() {
     <>
     <HippoBank title="Savings Hippo"
                info="A good place for your money" />
-
-    <HippoBank title="Savings Hippo 2"
-               info="A not so very good place for your money" />
     </>
   );
 }
--- src/HippoBank.js
+++ src/HippoBank.js
@@ -4,9 +4,18 @@ import React, {useState} from 'react';
 // const HippoBank = (props) => {   - 'old' way
 const HippoBank = ({title, info}) => {
 
-    // useState(0) is the default value for total
-    // setTotal is function to add behaviour for total
+    // total: is a variable (or const) name
+    // setTotal: is a function to add behaviour for total
+    // useState(0): is a two-element array and also contains the default value for total
     const [total, setTotal] = useState(0);
+
+    // Needs to be in an anonymous function so that it isn't 
+    // automatically called (without it being referenced). As
+    // we are not passing any arguments to this function, we can
+    // call it below without any brackets...
+    const deposit = () => {
+        setTotal(total + 10);
+    }
 
     return (
 
@@ -17,6 +26,8 @@ const HippoBank = ({title, info}) => {
         <>
             <h1>{title}</h1>
             <p>{info}</p>
+            <p>Total: £{total}</p>
+            <button onClick={deposit}>Deposit</button>
         </>
     )
 }
